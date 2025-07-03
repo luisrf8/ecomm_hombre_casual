9b462b96fb110249047cf0f1860b50ab65fc1b69
luisrf8 <117134650+luisrf8@users.noreply.github.com>
2025-07-03 17:04:10 -0400

    footer

--- components/layout/footer-menu.jsx
+++ components/layout/footer-menu.jsx
@@ -2,53 +2,31 @@
 import clsx from 'clsx';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
-
+import { FacebookIcon, InstagramIcon, TelegramIcon, WhatsAppIcon } from '../icons/svg/socialMedia';
 const fontStyle = {
-  color: '#022368',
+  color: 'white',
   fontWeight: 'bold',
-  fontSize: '1.1rem'
-}
+  fontSize: '1.1rem',
+};
+
 const menuOptions = [
   {
     id: 'Nosotros',
     title: <span style={fontStyle}>Nosotros</span>,
     path: '/',
-    submenus: [
-      { id: 'opcion1', title: '¿Quienes Somos?', path: '/opcion1' },
-      { id: 'opcion2', title: 'Mapa del Sitio', path: '/opcion2' },
-      { id: 'opcion3', title: 'Nosotros', path: '/opcion3' },
-    ],
+    submenus: [{ id: 'opcion2', title: 'Mapa del Sitio', path: '/opcion2' }],
   },
   {
     id: 'Ayuda',
     title: <span style={fontStyle}>Ayuda</span>,
     path: '/about',
-    submenus: [
-      { id: 'acerca-opcion1', title: 'Preguntas Frecuentes', path: '/about/opcion1' },
-      { id: 'acerca-opcion2', title: 'Contacto', path: '/about/opcion2' },
-      // Agregar más submenús según sea necesario
-    ],
+    submenus: [{ id: 'acerca-opcion2', title: 'Contacto', path: '/about/opcion2' }],
   },
   {
     id: 'Legal',
     title: <span style={fontStyle}>Legal</span>,
     path: '/services',
-    submenus: [
-      { id: 'acerca-opcion3', title: 'Términos y condiciones', path: '/about/opcion1' },
-      { id: 'acerca-opcion4', title: 'Acuerdos de Privacidad', path: '/about/opcion1' },
-      // Submenús para Servicios
-    ],
-  },
-  {
-    id: 'Info',
-    title: <span style={fontStyle}>Aliados</span>,
-    path: '/contact',
-    // submenus: [
-    //   { id: 'acerca-opcion5', title: 'ZuliaGlass', path: '/about/opcion1' },
-    //   { id: 'acerca-opcion6', title: 'BariGlass', path: '/about/opcion1' },
-    //   { id: 'acerca-opcion7', title: 'LaraGlass', path: '/about/opcion1' },
-    //   // Submenús para Contacto
-    // ],
+    submenus: [{ id: 'acerca-opcion3', title: 'Términos y condiciones', path: '/about/opcion1' }],
   },
 ];
 
@@ -57,23 +35,18 @@ function FooterMenuItem({ item }) {
   const [showSubmenus, setShowSubmenus] = useState(true);
 
   useEffect(() => {
-    // Check the pathname on the client side
     const pathname = window.location.pathname;
     setActive(pathname === item.path);
   }, [item.path]);
 
-  const toggleSubmenus = () => {
-    setShowSubmenus(!showSubmenus);
-  };
+  const toggleSubmenus = () => setShowSubmenus(!showSubmenus);
 
   return (
     <li>
       <div
         className={clsx(
-          'block p-2 text-lg underline-offset-4 hover:text-black hover:underline  md:inline-block md:text-sm',
-          {
-            'text-black': isActive,
-          }
+          'block p-2 text-lg underline-offset-4 hover:text-black hover:underline md:inline-block md:text-sm',
+          { 'text-black': isActive }
         )}
         onClick={toggleSubmenus}
       >
@@ -83,10 +56,8 @@ function FooterMenuItem({ item }) {
         <ul>
           {item.submenus.map((submenuItem) => (
             <li key={submenuItem.id}>
-              <Link href={submenuItem.path} passHref>
-                <div className="p-2 md:text-sm hover:text-black">
-                  {submenuItem.title}
-                </div>
+              <Link href={submenuItem.path}>
+                <div className="p-2 md:text-sm hover:text-black">{submenuItem.title}</div>
               </Link>
             </li>
           ))}
@@ -98,12 +69,31 @@ function FooterMenuItem({ item }) {
 
 export default function FooterMenu() {
   return (
-    <nav>
-      <ul className='md:flex md:gap-10'>
-        {menuOptions.map((item) => (
-          <FooterMenuItem key={item.id} item={item} />
-        ))}
-      </ul>
-    </nav>
+    <div className="flex flex-col md:flex-row md:items-start md:justify-between w-full gap-5 mt-4">
+      {/* Menú */}
+      <nav>
+        <ul className="md:flex md:gap-10">
+          {menuOptions.map((item) => (
+            <FooterMenuItem key={item.id} item={item} />
+          ))}
+        </ul>
+      </nav>
+
+      {/* Redes sociales */}
+      <div className="flex gap-8 text-[#022368] mt-4 md:mt-10">
+        <a href="https://www.instagram.com/infinitycenter.ca/" target="_blank" rel="noopener noreferrer" className='text-[10px]'>
+          <InstagramIcon  className="w-10 h-10" />
+        </a>
+        <a href="https://api.whatsapp.com/send?phone=584122628765" target="_blank" rel="noopener noreferrer">
+          <WhatsAppIcon  className="w-10 h-10" />
+        </a>
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" style={{ color: '#022368', fontSize: '0.5rem' }}>
+          <FacebookIcon  className="w-10 h-10" />
+        </a>
+        <a href="https://t.me" target="_blank" rel="noopener noreferrer">
+          <TelegramIcon  className="w-10 h-10" />
+        </a>
+      </div>
+    </div>
   );
 }
--- components/layout/footer.jsx
+++ components/layout/footer.jsx
@@ -6,8 +6,8 @@ export default function Footer() {
   const skeleton = 'w-full h-6 animate-pulse rounded bg-neutral-200 dark:bg-neutral-700';
 
   return (
-    <footer className="text-sm text-neutral-500 bg-gray-200">
-      <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 px-6 py-12 text-sm dark:border-neutral-700 md:flex-row md:gap-12 md:px-4 min-[1320px]:px-0">
+    <footer className="text-sm text-neutral-500 bg-black">
+      <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 px-6 pt-4 text-sm dark:border-neutral-700 md:flex-row md:gap-12 md:px-4 min-[1320px]:px-0">
         <Suspense
           fallback={
             <div className="flex h-[188px] w-[200px] flex-col gap-2">
@@ -23,9 +23,8 @@ export default function Footer() {
           <FooterMenu />
         </Suspense>
       </div>
-      <div className="border-t border-neutral-200 px-6 py-6">
+      <div className="px-6 py-6">
         <div className="mx-auto md:flex w-full max-w-7xl xs:text-[0.75rem] dark:border-neutral-700 min-[1320px]:px-0">
-          <hr className="hidden h-4 border-neutral-400 md:inline-block text-white"/>
           <p className="text-white">2024 Infinity Center C.A. | Todos los derechos reservados</p>
         </div>
       </div>
